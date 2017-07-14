6d179bc5c94a9be79c53fd8a18c704beef3b13f9
FukuiAyane <ayane053011@gmail.com>
2017-07-14 11:37:26 +0900

    structural reforms

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 $(function(){
   $('#menu_unabout').click(function(){
     $('#panel_submenu_unabout').show();
@@ -13,25 +15,21 @@ $(function(){
   })
   $(document).on('click touchend',   function(e) {
   if (!$(e.target).closest('.point-day').length) {
-    // 処理方法を記述;
     $('.cal_submenu').hide();
   }
   });
   $(document).on('click touchend',   function(e) {
   if (!$(e.target).closest('#menu_unabout').length) {
-    // 処理方法を記述;
     $('#panel_submenu_unabout').hide();
   }
   });
   $(document).on('click touchend',   function(e) {
   if (!$(e.target).closest('#menu_prabout').length) {
-    // 処理方法を記述;
     $('#panel_submenu_prabout').hide();
   }
   });
   $(document).on('click touchend',   function(e) {
   if (!$(e.target).closest('#menu_detail').length) {
-    // 処理方法を記述;
     $('#panel_submenu_detail').hide();
   }
   });
