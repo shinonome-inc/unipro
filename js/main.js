d7bf917eaa744cf12d428961d6555c4783f5e5de
FukuiAyane <ayane053011@gmail.com>
2017-08-17 01:22:27 +0900

    delete rc

--- js/main.js
+++ js/main.js
@@ -42,3 +42,17 @@ $(function() {
         $('#sky').unwrap();
     }
 });
+
+
+$(function() {
+  $("a[href*=#]:not([href=#])").click(function(){
+
+    var target = $($(this).attr("href")).offset().top;
+
+    target -= 97;
+
+    $("html, body").animate({scrollTop: target}, 500);
+
+    return false;
+  });
+});
