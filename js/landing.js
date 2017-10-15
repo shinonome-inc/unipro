c15e2622262fb2dfeffb63faa95dc780e353d84a
FukuiAyane <ayane053011@gmail.com>
2017-10-16 01:32:53 +0900

    delete unnecessary

--- js/landing.js
+++
@@ -1,9 +0,0 @@
-// $(function() {
-//   $("button.more-read").hover(function(){
-
-//     target = $(this).parent().parent().css("box-shadow", "0 27px 24px 0 rgba(0, 0, 0, 0.2)");
-
-//   },function(){
-//     target = $(this).parent().parent().css("box-shadow", "0 2px 5px 0 rgba(0, 0, 0, 0.16)");
-//   });
-// });
